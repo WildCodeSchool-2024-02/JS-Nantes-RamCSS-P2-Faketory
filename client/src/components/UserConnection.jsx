bbe8288411b9b13ff11b9c99aa93e4981cf9d235
stevenbachimont <stevenbachimont@gmail.com>
2024-04-17 16:19:34 +0200

    S02.2_userConnect: améliorations implatations

--- client/src/components/UserConnection.jsx
+++ client/src/components/UserConnection.jsx
@@ -1,17 +1,19 @@
 import { useState, useEffect } from "react";
-import newsData from "../Fakenews.json"; 
+import newsData from "../Fakenews.json";
 
 import "./UserConnection.css";
 
 function UserConnection() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [newsArticle, setNewsArticle] = useState(null);
+  const [newsArticle2, setNewsArticle2] = useState(null);
+  const [newsArticle6, setNewsArticle6] = useState(null);
 
   useEffect(() => {
-    // eslint-disable-next-line no-shadow
-    const article = newsData.fakenews.find((article) => article.id === 2);
-    setNewsArticle(article);
+    const article2 = newsData.fakenews.find((article) => article.id === 2);
+    const article6 = newsData.fakenews.find((article) => article.id === 6);
+    setNewsArticle2(article2);
+    setNewsArticle6(article6);
   }, []);
 
   const handleSubmit = (event) => {
@@ -19,53 +21,57 @@ function UserConnection() {
   };
 
   return (
-      <>
-        <h2>{username || "User Connection"}</h2>
-        <h3> WELCOME </h3>
-        <div id="cards" className="cards">
-          {newsArticle && (
-              <div id="card2" className="card">
-                <h4>{newsArticle.title}</h4>
-                <p>{newsArticle.body}</p>
-              </div>
-          )}
-
-          <div id="card1" className="card">
-            <form onSubmit={handleSubmit}>
-              <label>
-                <h5>Nom d'utilisateur :</h5>
-                <input
-                    type="text"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                />
-              </label>
-              <label>
-                <h5> Mot de passe :</h5>
-                <input
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-              </label>
-              <button type="button" className="submit">
-                Se connecter
-              </button>
-              <h5>Vous n'avez pas de compte ?</h5>
-              <a href="/register">Créer un compte</a>
-            </form>
+    <>
+      <h2>{username || "User Connection"}</h2>
+      <h3> WELCOME </h3>
+      <div id="cards" className="cards">
+        {newsArticle2 && (
+          <div id="card2" className="card">
+            <h4>
+              {username || "User Connection"}
+              {newsArticle2.title}
+            </h4>
+            <p>{newsArticle2.body}</p>
           </div>
+        )}
 
-          <div id="card3" className="card">
-            <h4>8 ans de prison pour {username || "Jacky LAMBROUILLE"}</h4>
-            <p>
-              “Neque porro quisquam est, ipsum quia dolor sit amet, consectetur,
-              adipisci velit, sed quia non numquam eius modi tempora incidunt ut
-              labore et dolore magnam aliquam quaerat voluptatem.”
-            </p>
-          </div>
+        <div id="card1" className="card">
+          <form onSubmit={handleSubmit}>
+            <label>
+              <h5>Nom d'utilisateur :</h5>
+              <input
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </label>
+            <label>
+              <h5> Mot de passe :</h5>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </label>
+            <button type="button" className="submit">
+              Se connecter
+            </button>
+            <h5>Vous n'avez pas de compte ?</h5>
+            <a href="/register">Créer un compte</a>
+          </form>
         </div>
-      </>
+
+        {newsArticle6 && (
+          <div id="card3" className="card">
+            <h4>
+              {username || "User Connection"}
+              {newsArticle6.title}
+            </h4>
+            <p>{newsArticle6.body}</p>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
