433e8d0ac59d6cecbdabfb4f38329d92b758234e
Adeline44 <adeline.cornier@hotmail.com>
2024-04-19 11:14:10 +0200

    S02_NavBar CSS en cours

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -3,18 +3,17 @@ import { NavLink } from "react-router-dom";
 
 function NavBar() {
   return (
-    <header>
-      <div className="header">
-        <nav className="nav container">
-          <ul>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/best-of">Best Of</NavLink>
-            <NavLink to="/create">Creer sa Fake News</NavLink>
-            <NavLink to="/fakeschool">Fake School</NavLink>
-            <NavLink to="/connexion">Connexion</NavLink>
-          </ul>
-        </nav>
-      </div>
+    <header className="header">
+      <img src="./images/logo-faketory.svg" alt="logo faketory" />
+      <nav className="nav container">
+        <ul>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/best-of">Best Of</NavLink>
+          <NavLink to="/create">Creer sa Fake News</NavLink>
+          <NavLink to="/fakeschool">Fake School</NavLink>
+          <NavLink to="/connexion">Connexion</NavLink>
+        </ul>
+      </nav>
     </header>
   );
 }
