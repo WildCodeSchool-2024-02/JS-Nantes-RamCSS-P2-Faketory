fd1503b8f415fb69d8d47b5af8fa88ba0335e408
Adeline44 <adeline.cornier@hotmail.com>
2024-04-24 10:05:22 +0200

    S03-NavBar en cours + footer en cours

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,36 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <article>
+      <ul className="contact">
+        <li> Contact </li>
+        <li> Annonceur </li>
+        <li> CGU </li>
+        <li> Charte sur les données personnelles </li>
+        <li> Politique cookies </li>
+        <li> Gestion des cookies </li>
+      </ul>
+      <ul className="navigation">
+        <li> NAVIGATION </li>
+        <li> Home </li>
+        <li> Best Of </li>
+        <li> Créer sa Fake News </li>
+        <li> Fake School </li>
+        <li> Connexion </li>
+      </ul>
+      <p>
+        {" "}
+        Copyright © - 2024 - Tous droits réservés. Politique de confidentialité
+        - Mentions légales.
+      </p>
+
+      <span className="icon-return" />
+      <button type="button">
+        <img src="./images/logo-faketory.svg" alt="logo faketory" />
+      </button>
+    </article>
+  );
+}
+
+export default Footer;
