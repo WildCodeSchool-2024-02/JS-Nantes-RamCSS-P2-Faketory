ba382b824c933d4957ec597b44ef8c1f921787ee
Hakim AZIZI <hakazizi@hotmail.com>
2024-04-26 22:42:54 +0200

    ajout des lignes sans fonction pour afficher aleatoirement les news ou les fakenews

--- client/src/pages/SelectNews.jsx
+++ client/src/pages/SelectNews.jsx
@@ -1,20 +1,36 @@
 import { useState, useEffect } from "react";
-
 import "./select-news.css";
 
 function SelectNews() {
   const [content, setContent] = useState([]);
-
+  const [content1, setContent1] = useState([]);
   useEffect(() => {
     fetch("http://localhost:3000/src/news.json")
       .then((response) => response.json())
-      .then((data) => setContent(data));
+      .then((data) => setContent(data.news));
   }, []);
-
-  if (content.length === 0) {
+  useEffect(() => {
+    fetch("http://localhost:3001/api/fakenews")
+      .then((response) => response.json())
+      .then((data) => setContent1(data.fakenews));
+  }, []);
+  if (content.length === 0 || content1.length === 0) {
     return "Chargement...";
   }
-
+  const choice = Math.floor(Math.random() * 2);
+  const checknews = [];
+  if (choice === 0) {
+    checknews.push(content);
+  } else {
+    checknews.push(content1);
+  }
+  if (choice === 1) {
+    checknews.push(content);
+  } else {
+    checknews.push(content1);
+  }
+  const contentLeft = checknews[0];
+  const contentRight = checknews[1];
   return (
     <>
       <header className="select-news">
@@ -22,25 +38,24 @@ function SelectNews() {
         <h2>that is the question</h2>
       </header>
       <section className="selectnews">
-      <article>
-          <img src={content[0].img} alt="" />
+        <article>
+          <img src={contentLeft[0].img} alt="" />
           <h2>
-            {content[2].name}
-            {content[2].title}
+            {contentLeft[0].name}
+            {contentLeft[0].title}
           </h2>
           <p>&nbsp;</p>
         </article>
         <article>
-          <img src={content[0].img} alt="" />
+          <img src={contentRight[0].img} alt="" />
           <h2>
-            {content[2].name}
-            {content[2].title}
+            {contentRight[0].name}
+            {contentRight[0].title}
           </h2>
-          <p>{content[2].body}</p>
+          <p>&nbsp;</p>
         </article>
       </section>
     </>
   );
 }
-
 export default SelectNews;
