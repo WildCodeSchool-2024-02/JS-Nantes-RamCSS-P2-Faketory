21b19741d0516bb5bb836f5f7cadab45a50fb114
lukasz <zed11temp@gmail.com>
2024-05-11 01:44:06 +0200

    fix path images in ImagesTest.jsx

--- client/src/components/ImagesTest.jsx
+++ client/src/components/ImagesTest.jsx
@@ -4,20 +4,20 @@ function ImagesTest() {
   return (
     <div className="images">
       <figcaption>
-        <img src="./public/images/loto.jpg" alt="loto" height="150" />
+        <img src="images/loto.jpg" alt="loto" height="150" />
       </figcaption>
 
       <figcaption>
-        <img src="./public/images/JO 2024.jpg" alt="jo-2024" width="260" />
+        <img src="images/JO 2024.jpg" alt="jo-2024" width="260" />
       </figcaption>
 
       <figcaption>
-        <img src="./public/images/pape.jpg" alt="pape" height="150" />
+        <img src="images/pape.jpg" alt="pape" height="150" />
       </figcaption>
 
       <figcaption>
         <img
-          src="./public/images/Train Japon.jpg"
+          src="images/Train Japon.jpg"
           alt="train-japon"
           height="150"
         />
